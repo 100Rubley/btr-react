dc3d9dbd7beb0bcc0369abfa9012032413604031
Anton Kazakov <toxa_98@inbox.ru>
2023-06-06 18:39:07 +0300

    add index.scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,18 @@
-import { useEffect } from "react";
-import Box from "../Box/Box";
-import "./app.scss";
+import { useEffect } from 'react';
+import Box from '../Box/Box';
 
 const currentWindow = window as any;
 const tg = currentWindow.Telegram.WebApp;
 
 export const App = () => {
-
   useEffect(() => {
     tg.ready();
   }, []);
-  
+
   return (
-    <>
-      <div className="app">asda</div>
-      <Box />;
-    </>
+    <div>
+      <Box />
+      <button> toggle</button>
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/App';
-import './index.module.css';
+import { App } from './components/App/App';
+import './styles/index.scss';
 
 ReactDOM.render(<App />, document.querySelector('#app'));
 
