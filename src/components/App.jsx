2cbaef217aeea9bd6678cc802186ded68e5f621c
Елизавета Ксенофонтова <elizavetaksenofontova@MacBook-Air-Elizaveta.local>
2023-06-06 16:42:17 +0300

    check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './App.module.css';
 
 export const App = () => {
-  const title = 'React + Webpack и babel';
+  const title = 'React + Webpack и babel77777777777777777777knletenfgkjdfnbkdfnbldfnbldnfbl/jkdnf/bkjdbgdfnlk';
 
   return <h1 className={s.title}>{title}</h1>;
 };
