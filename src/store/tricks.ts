d836a868bbaf8d87809f15b7800854e5feacf4da
Anton Kazakov <toxa_98@inbox.ru>
2023-06-07 17:37:09 +0300

    add tricks store

---
+++ src/store/tricks.ts
@@ -0,0 +1,33 @@
+interface ISpinsObject {
+  easy: String[];
+  medium: String[];
+  hard: String[];
+  impossible: String[];
+}
+
+interface ITricksObject {
+  soul: String[];
+  groove: String[];
+}
+
+// negative top side switch - будут в коде выглядеть как флаг true/false, там названия не нужны
+
+export const entrances: ISpinsObject = {
+  easy: ['No spin', 'Inspin', 'Outspin'],
+  medium: ['Zerospin', 'Alleyoop'],
+  hard: ['Truespin', 'Full Cab', 'Full Cab true', '270', 'Hurricane'],
+  impossible: ['450', '540', '720', 'Misty to', 'Back flip to']
+};
+
+export const tricks: ITricksObject = {
+  soul: ['Soul', 'Mizou', 'Acid', 'PStar', 'Makio', 'X-Grind', 'Mistrial', 'Torque soul'],
+  groove: ['UFO', 'Royale', 'Full Torque', 'Unity', 'Backslide', 'Torque', 'Cab driver', 'Fastslide', 'Pudslide'],
+};
+
+export const spins: ISpinsObject = {
+  easy: ['No spin', '180 Easy way'],
+  medium: ['180 Hard way', '270', '360'],
+  hard: ['540', '720'],
+  impossible: ['Misty out', 'Back flip out']
+};
+
