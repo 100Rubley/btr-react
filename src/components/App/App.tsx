a46c6d9ba824db5e2e962da5d869876f9aaed060
Anton Kazakov <toxa_98@inbox.ru>
2023-06-29 15:43:35 +0300

    add input type range styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,6 @@ import s from "./App.module.scss";
 import Rampa from "../../assets/rampa.svg";
 import Toggle from "../../assets/toggle.svg";
 import { useTheme } from "../../styles/theme/useTheme";
-import { LevelSelector } from "../LevelSelector/LevelSelector";
 
 const currentWindow = window as any;
 const tg = currentWindow.Telegram.WebApp;
@@ -23,7 +22,6 @@ export const App = () => {
         </div>
         <Toggle onClick={toggleTheme} />
       </div>
-      <LevelSelector />
       <Rampa />
     </div>
   );
